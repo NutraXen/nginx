358eac44c9f5c1783cdd405371b2156d96160a2b
Frank <frankbasti@outlook.com>
2013-09-19 17:41:35 -0700

    Full Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+nginx
+=====
+
+<h2>Download, Build & Install NginX custom: <br>Azure / Aws Servers</h2>
+<ul>
+	<li>Creates /XEN-TOOLS/ director at root of server</li>
+	<li>Creates custom install of nginx</li>
+	<li>Pulls sites / vhosts from within this repo & configures them</li>
+</ul>
+
+<h3>Instructions</h3>
+<ul>
+	<li>Fresh install Ubuntu 12.04 on Azure Server</li>
+	<li>Become Root by typing: sudo su </li>
+	<li>Move to temp directory: cd /tmp </li>
+	<li>Download installer script: wget https://raw.github.com/villanus/nginx/master/bash/install-nginx-custom.sh</li>
+	<li>Begin Installer: sudo bash install-nginx-custom.sh</li>
+</ul>
+
+<h5>Important!</h5>
+<i>Note: The git repository /usr/share/nginx/ is removed after script completes.<br>
+to get latest version type the following commands:<br><br>
+service nginx stop<br>
+cd /usr/share<br>
+rm -r nginx<br>
+git clone https://github.com/villanus/nginx.git<br>
+/etc/init.d/nginx reload<br>
+service nginx start<br>
+</i>
---
+++ bash/install-nginx-custom.sh
@@ -0,0 +1,324 @@
+#!/bin/bash
+# INSPIRED BY:
+# https://github.com/evansims/scripts/blob/master/build-scripts/ubuntu12.04/nginx-pagespeed.sh
+
+
+# QUICK INSTALL INSTRUCTIONS:
+# cd /tmp
+# wget https://raw.github.com/villanus/nginx/master/bash/install-nginx-custom.sh
+# sudo bash install-nginx-custom.sh
+
+
+
+
+# Customize your build flags here.
+export CFLAGS="-pipe -march=nocona -mtune=i686 -O2 -msse -mmmx -msse2 -msse3 -mfpmath=sse"
+
+# Determine the number of processors available to us for configuring scripts.
+CORES=`grep processor /proc/cpuinfo | wc -l`
+if ! [[ "$CORES" =~ ^[0-9]+$ ]] || [[ "$CORES" -lt 1 ]]; then
+        CORES=1
+fi
+
+# GET UPDATES 
+sudo apt-get update && sudo apt-get autoremove && sudo apt-get autoclean
+sudo apt-get -y install htop unzip git build-essential libpcre3 libpcre3-dev libssl-dev checkinstall automake
+
+# TODO:
+# INSTALL PHP
+# INSTALL MYSQL
+
+
+
+
+#/backup firewall/
+cd /
+mkdir XEN-TOOLS
+cd XEN-TOOLS
+mkdir BASH-SCRIPTS
+cd /XEN-TOOLS/BASH-SCRIPTS
+cd /XEN-TOOLS
+mkdir FIREWALL
+echo "created directories /XEN-TOOLS"
+iptables-save > /XEN-TOOLS/FIREWALL/firewall.conf
+iptables-save > /XEN-TOOLS/FIREWALL/firewall.original.conf
+echo "Firewall Backed Up"
+cd /XEN-TOOLS
+mkdir SSL-BACKUPS
+
+
+sudo apt-get update
+sudo apt-get -y install zip
+sudo apt-get -y install build-essential libxml2-dev libfuse-dev libcurl4-openssl-dev
+sudo apt-get -y install htop unzip git build-essential libpcre3 libpcre3-dev libssl-dev checkinstall automake
+
+
+echo ""
+echo "Updates Completed!"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+
+
+# Create directory for Src files.
+cd /
+mkdir ~/src
+
+# Grab upload progress module.
+cd ~/src
+git clone https://github.com/masterzen/nginx-upload-progress-module
+
+# Grab more-headers module.
+git clone https://github.com/agentzh/headers-more-nginx-module
+
+# Grab the uptsream fair module.
+git clone https://github.com/gnosek/nginx-upstream-fair
+
+# Grab the Upstream Module
+git clone https://github.com/yaoweibin/nginx_upstream_check_module
+
+# Grab NginX Substitutions Module
+git clone https://github.com/yaoweibin/ngx_http_substitutions_filter_module
+
+
+# Grab libunwind
+wget http://download.savannah.gnu.org/releases/libunwind/libunwind-0.99-beta.tar.gz
+tar -xzvf libunwind-0.99-beta.tar.gz
+rm libunwind-0.99-beta.tar.gz
+cd libunwind-0.99-beta
+sudo ./configure CFLAGS="-U_FORTIFY_SOURCE -pipe -march=nocona -mtune=i686 -O2 -msse -mmmx -msse2 -msse3 -mfpmath=sse" && sudo make && sudo checkinstall --pkgversion=0.99 --default
+
+# Grab Google's Performance Tools library.
+cd ~/src
+wget https://gperftools.googlecode.com/files/gperftools-2.0.tar.gz
+tar -xzvf gperftools-2.0.tar.gz
+rm gperftools-2.0.tar.gz
+cd gperftools-2.0
+sudo ./configure CFLAGS="-pipe -march=nocona -mtune=i686 -O2 -msse -mmmx -msse2 -msse3 -mfpmath=sse" && sudo make && sudo checkinstall --default
+
+# Grab Google's Pagespeed module.
+#cd ~/src
+#wget https://github.com/pagespeed/ngx_pagespeed/archive/release-1.6.29.5-beta.zip
+#unzip release-1.6.29.5-beta.zip
+#rm release-1.6.29.5-beta.zip
+#cd ngx_pagespeed-release-1.6.29.5-beta/
+#wget https://dl.google.com/dl/page-speed/psol/1.6.29.5.tar.gz
+#tar -xzvf 1.6.29.5.tar.gz
+#rm 1.6.29.5.tar.gz
+
+
+echo "Downloaded Necessary Nginx Plugins"
+echo ""
+echo ""
+echo ""
+echo ""
+echo "COMILING NGINX"
+echo ""
+echo ""
+
+
+# Compile Nginx.
+cd ~/src
+wget http://nginx.org/download/nginx-1.4.2.tar.gz
+tar -xzvf nginx-1.4.2.tar.gz
+rm nginx-1.4.2.tar.gz
+cd nginx-1.4.2
+
+# REQUIRED PATCH FOR UPSTREAM 
+sudo patch -p1 < ~/src/nginx_upstream_check_module/check_1.2.6+.patch
+
+sudo ./configure \
+--prefix=/usr/share/nginx \
+--sbin-path=/usr/sbin/nginx \
+--conf-path=/etc/nginx/nginx.conf \
+--pid-path=/run/nginx.pid \
+--error-log-path=/var/log/nginx/error.log \
+--http-log-path=/var/log/nginx/access.log \
+--http-client-body-temp-path=/var/lib/nginx/body \
+--http-proxy-temp-path=/var/lib/nginx/proxy \
+--http-fastcgi-temp-path=/var/lib/nginx/fastcgi \
+--lock-path=/var/lock/nginx.lock \
+--with-cc-opt="-pipe -march=nocona -O2 -msse -mmmx -msse2 -msse3 -mfpmath=sse" \
+--with-rtsig_module \
+--without-select_module \
+--without-poll_module \
+--with-file-aio \
+--with-ipv6 \
+--with-http_ssl_module \
+--with-http_spdy_module \
+--with-http_realip_module \
+--with-http_mp4_module \
+--with-http_gunzip_module \
+--without-http_ssi_module \
+--without-http_userid_module \
+--without-http_geo_module \
+--without-http_referer_module \
+--without-http_uwsgi_module \
+--without-http_scgi_module \
+--without-http_memcached_module \
+--without-http_empty_gif_module \
+--without-http_browser_module \
+--with-google_perftools_module \
+--with-pcre-jit \
+--add-module=$HOME/src/nginx-upload-progress-module \
+--add-module=$HOME/src/headers-more-nginx-module/ \
+--add-module=$HOME/src/ngx_http_substitutions_filter_module/ \
+--add-module=$HOME/src/nginx_upstream_check_module/ && sudo make && sudo checkinstall --default
+
+sudo /sbin/ldconfig
+
+# Configure nginx
+sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.custom.conf
+sudo sed -i "s/worker_processes  1;/worker_processes $CORES;/" /etc/nginx/nginx.custom.conf
+
+# Install init.d script
+sudo wget https://raw.github.com/JasonGiedymin/nginx-init-ubuntu/master/nginx -O /etc/init.d/nginx
+sudo chmod +x /etc/init.d/nginx
+sudo sed -i 's/DAEMON=\/usr\/local\/nginx\/sbin\/nginx/DAEMON=\/usr\/sbin\/nginx/' /etc/init.d/nginx
+sudo sed -i 's/lockfile=\/var\/lock\/subsys\/nginx/lockfile=\/var\/lock\/nginx.lock/' /etc/init.d/nginx
+sudo sed -i 's/PIDSPATH=\/usr\/local\/nginx\/logs/PIDSPATH=\/run/' /etc/init.d/nginx
+sudo sed -i 's/NGINX_CONF_FILE="\/usr\/local\/nginx\/conf\/nginx.conf"/NGINX_CONF_FILE="\/etc\/nginx\/nginx.custom.conf"/' /etc/init.d/nginx
+sudo update-rc.d -f nginx defaults
+
+sudo mkdir /var/lib/nginx
+sudo mkdir /var/lib/nginx/body
+sudo mkdir /etc/nginx/conf.d
+sudo mkdir /var/cache/nginx
+sudo mkdir /var/cache/nginx/ngx_pagespeed_cache
+
+
+
+sudo mkdir /usr/share/nginx/cache
+sudo mkdir /usr/share/nginx/cache/a
+sudo mkdir /usr/share/nginx/cache/b
+sudo mkdir /usr/share/nginx/cache/c
+sudo mkdir /usr/share/nginx/cache/d
+sudo mkdir /usr/share/nginx/cache/e
+
+
+
+sudo service nginx start
+
+
+echo ""
+echo ""
+echo "NginX Installed and Started"
+echo ""
+echo "######################################################"
+echo "# SETTING UP INTIAL SITES / CONFIGURATION FOR NGINX"
+echo "#######################################################"
+echo ""
+echo "Stopping NginX"
+service nginx stop
+
+# remove old conf file & create working directories
+cd /etc/nginx/ 
+cp nginx.conf.default nginx.custom.oirginal
+rm nginx.conf.default
+rm nginx.conf
+mkdir /etc/nginx/sites-enabled/
+mkdir /etc/nginx/sites-available/
+echo "Directories Created"
+echo ""
+
+# Clone Default Site from Github / Add it to sites available and enabled.  
+cd /usr/share/
+rm -r /usr/share/nginx/
+git clone https://github.com/villanus/nginx.git
+echo "Cloned Git Repo - NGINX"
+echo ""
+
+
+# Replace Nginx Config with the one from server
+echo "Replacing Nginx Configuration"
+cd /etc/nginx/
+rm nginx.custom.conf
+echo "- Removed /etc/nginx/nginx.custom.conf"
+cp /usr/share/nginx/etc-config/nginx.custom.conf /etc/nginx/nginx.custom.conf
+echo "- Replaced /etc/nginx/nginx.custom.conf with configuration from /usr/share/nginx/etc-config"
+echo "-- Nginx now using custom configuraiton from Git Repo"
+echo ""
+echo ""
+cp nginx.custom.conf nginx.custom.villanus
+
+
+
+# Symlink the config files for default site...
+echo ""
+echo "-- Sites Enabled > azure-default-site"
+rm /etc/nginx/sites-available/azure-default-site.conf
+ln -s  /usr/share/nginx/vhosts/azure-default-site.conf /etc/nginx/sites-available/azure-default-site.conf
+rm /etc/nginx/sites-enabled/azure-default-site.conf
+ln -s  /usr/share/nginx/vhosts/azure-default-site.conf /etc/nginx/sites-enabled/azure-default-site.conf
+
+# Symlink the config for Maintenence (Temp) Nutraxen Site + Reverse Proxy!
+echo ""
+echo "-- Sites Enabled > www-nutraxe-com"
+rm /etc/nginx/sites-available/www-nutraxen-com.conf
+ln -s  /usr/share/nginx/vhosts/www-nutraxen-com.conf /etc/nginx/sites-available/www-nutraxen-com.conf
+rm /etc/nginx/sites-enabled/www-nutraxen-com.conf
+ln -s  /usr/share/nginx/vhosts/www-nutraxen-com.conf /etc/nginx/sites-enabled/www-nutraxen-com.conf
+
+
+
+
+echo ""
+echo "######################################################"
+echo "#    CLEANING UP AND CREATING XEN-TOOLS SHORTCUTS"
+echo "#######################################################"
+echo ""
+# Remove git from directory???
+echo "Cleaning Up:"
+#echo " - Removing Git Directory /usr/share/nginx/nginx-default-site/.git/"
+#rm -r /usr/share/nginx/.git/ 
+echo " - Moving installer script to /XEN-TOOLS/BASH-SCRIPTS/install-nginx-custom.sh"
+cp /tmp/install-nginx-custom.sh /XEN-TOOLS/BASH-SCRIPTS/install-nginx-custom.sh
+echo ""
+
+# Create Shortcuts
+echo "Creating Shortcuts:"
+echo " - XEN-TOOLS/NGINX - parent folder"
+cd /XEN-TOOLS/
+mkdir NGINX
+echo "- XEN-TOOLS/NGINX/server.config - Server Confguration File"
+ln -s  /etc/nginx/nginx.custom.conf /XEN-TOOLS/NGINX/server.config
+echo "- XEN-TOOLS/NGINX/SERVER - shortcut: /etc/nginx"
+ln -s  /etc/nginx/ /XEN-TOOLS/NGINX/SERVER
+echo "- XEN-TOOLS/NGINX/VHOSTS - shortcut: /usr/share/nginx/vhosts/ "
+ln -s  /usr/share/nginx/vhosts/ /XEN-TOOLS/NGINX/VHOSTS
+echo "- XEN-TOOLS/NGINX/SITES-ENABLED - shortcut: /etc/nginx/sites-enabled "
+ln -s  /etc/nginx/sites-enabled/ /XEN-TOOLS/NGINX/SITES-ENABLED
+echo "- XEN-TOOLS/NGINX/SITES - shortcut: /usr/share/nginx/sites/ "
+ln -s  /usr/share/nginx/sites/ /XEN-TOOLS/NGINX/SITES
+echo "- XEN-TOOLS/NGINX/_LOGS - shortcut: /var/log/nginx/ "
+ln -s  /var/log/nginx/ /XEN-TOOLS/NGINX/_LOGS
+echo "- XEN-TOOLS/NGINX/SSL-CERTS - shortcut: /var/log/nginx/ "
+ln -s  /usr/share/nginx/ssl-certs/ /XEN-TOOLS/NGINX/SSL-CERTS
+ln -s  /usr/share/nginx/ssl-certs/ /etc/nginx/ssl-certs
+
+
+ln -s  /usr/share/nginx/cache /XEN-TOOLS/NGINX/CACHE
+
+# TODO CACHES
+
+echo ""
+echo ""
+echo "Starting Nginx"
+sudo /etc/init.d/nginx reload
+sudo service nginx restart
+echo ""
+echo "All Done!"
+
+echo "IMPORTANT!!!!"
+echo "- Please configure nginx sites:"
+echo "- changing directory to /XEN-TOOLS/NGINX/VHOSTS to get you started"
+cd /XEN-TOOLS/NGINX/VHOSTS
+
+echo "IMPORTANT!!!!"
+echo "- Please configure SSL Certificates As Required:"
+# Remove this bash script if loaded from tmp directory
+rm /tmp/install-nginx-custom.sh
