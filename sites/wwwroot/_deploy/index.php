cd88ac69306a3c072693219221e01df9282dae77
Frank <frankbasti@outlook.com>
2014-04-22 19:25:47 -0700

    Restart NginX after Git Commit

--- sites/wwwroot/_deploy/index.php
+++ sites/wwwroot/_deploy/index.php
@@ -1,5 +1,5 @@
 <?php
-shell_exec( 'cd /XEN-TOOLS/GIT-PULL-HERE/ && git reset --hard HEAD && git pull' );
+shell_exec( 'cd /XEN-TOOLS/GIT-PULL-HERE/ && git reset --hard HEAD && git pull && /etc/init.d/nginx reload ');
 ?>
 
 Done!
