59f3e950fce24140616e50cfadc30cf7d235b9c0
Frank <frankbasti@outlook.com>
2014-05-07 06:30:29 -0700

    Bash

--- reload.sh
+++ reload.sh
@@ -3,6 +3,9 @@ cd /XEN-TOOLS/GIT-PULL-HERE
 git pull
 rm -rf /usr/share/nginx/cache*
 /etc/init.d/nginx reload
+cd /var/log/nginx
+rm r *
+cd /XEN-TOOLS/GIT-PULL-HERE 
 echo ""
 echo "Nginx Reloaded wtih Latest GIT "
 echo ""
