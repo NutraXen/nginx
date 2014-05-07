2635284a59a194a1b34fc1a869c0a703740e1719
Frank <frankbasti@outlook.com>
2014-05-07 08:27:14 -0700

    bash script to reload nginx config and restart server after git pull

--- CRUNCH.sh
+++ CRUNCH.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 cd /XEN-TOOLS/GIT-PULL-HERE 
 git pull
+echo "*************************"
+echo "  1 - Git Pulled New Files"
+echo "*************************"
+
 rm -rf /usr/share/nginx/cache*
 /etc/init.d/nginx reload
 cd /var/log/nginx
 rm r *
+echo "*************************"
+echo "  2 - Clearned Nginx Cache"
+echo "*************************"
+
+cd /etc/nginx/
+rm nginx.custom.conf
+cp /usr/share/nginx/etc-config/nginx.custom.conf /etc/nginx/nginx.custom.conf
+echo "***********************************************"
+echo "  3 - Reloaded Nginx.custom.conf from repo"
+echo "***********************************************"
+
+
+
 cd /XEN-TOOLS/GIT-PULL-HERE 
 echo ""
-echo "Nginx Reloaded wtih Latest GIT "
+echo "DONE "
 echo ""
 echo ""
